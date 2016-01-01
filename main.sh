fe60eb48e6f954f2773e8df0216ca00d8c33df97
Himanshu Shekhar <himanshushekharb16@users.noreply.github.com>
2016-01-01 21:58:58 +0530

    Fixed bash issues

--- main.sh
+++ main.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# ProxyMan v1.1
+# ProxyMan v1.2
 # Author : Himanshu Shekhar < https://github.com/himanshushekharb16/ProxyMan >
 
 # This program is free software; you can redistribute it and/or modify

8ebaffea44130a8bd39dc9fa278982853ee70bd5
Himanshu Shekhar <himanshushekharb16@gmail.com>
2018-11-13 22:06:29 +0530

    fixed incorrect gsettings parameters

--- bash-zsh.sh
+++ bash-zsh.sh
@@ -12,8 +12,13 @@ fi
 
 if [ "$first" = 0 ]; then
     bash shellrc.sh "$1" "$HOME/.bashrc"
+    echo "To activate in current terminal window"
+    echo "run ${bold}source ~/.bashrc${normal}"
 fi
 
 if [ "$second" = 0 ]; then
     bash shellrc.sh "$1" "$HOME/.zshrc"
+    echo "To activate in current terminal window"
+    echo "run ${bold}source ~/.zshrc${normal}"
+    echo
 fi

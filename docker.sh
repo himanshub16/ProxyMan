a48932981797106b34967a1392589daab531b920
Himanshu Shekhar <himanshushekharb16@gmail.com>
2019-01-22 00:37:05 +0530

    fix identation

--- docker.sh
+++ docker.sh
@@ -7,52 +7,52 @@ CONF_FILE="/etc/systemd/system/docker.service.d/http-proxy.conf"
 
 list_proxy() {
     # inefficient way as the file is read twice.. think of some better way
-	echo -e "${bold}docker proxy settings: ${normal}"
+    echo -e "${bold}docker proxy settings: ${normal}"
     if [ ! -e "$CONF_FILE" ]; then
         echo -e "${red}None${normal}"
-		return
+        return
     else
         lines="$(cat $CONF_FILE | grep proxy -i | wc -l)"
-    	if [ "$lines" -gt 0 ]; then
-    		cat $CONF_FILE | grep proxy -i | sed -e "s/Environment=//g" -e "s/\_/\ /g"
-    	else
-    		echo -e "${red}None${normal}"
-    	fi
-	fi
+        if [ "$lines" -gt 0 ]; then
+            cat $CONF_FILE | grep proxy -i | sed -e "s/Environment=//g" -e "s/\_/\ /g"
+        else
+            echo -e "${red}None${normal}"
+        fi
+    fi
 }
 
 unset_proxy() {
     if [ ! -e "$CONF_FILE" ]; then
-		return
-	fi
-	if [ "$(cat $CONF_FILE | grep proxy -i | wc -l)" -gt 0 ]; then
-		sudo rm $CONF_FILE
+        return
+    fi
+    if [ "$(cat $CONF_FILE | grep proxy -i | wc -l)" -gt 0 ]; then
+        sudo rm $CONF_FILE
         sudo service docker restart
-	fi
+    fi
 }
 
 set_proxy() {
     unset_proxy
     if [ ! -e "$CONF_FILE" ]; then
-		sudo touch "$CONF_FILE"
-	fi
+        sudo touch "$CONF_FILE"
+    fi
 
     local stmt=""
     if [ "$use_auth" = "y" ]; then
         stmt="${username}:${password}@"
     fi
 
-	echo -n "" > docker_config.tmp
+    echo -n "" > docker_config.tmp
     echo "[Service]" >> docker_config.tmp
-	echo "Environment=\"HTTP_PROXY=http://${stmt}${http_host}:${http_port}\";" \
-        >> docker_config.tmp
-	echo "Environment=\"HTTP_PROXY=http://${stmt}${http_host}:${http_port}\";" \
-        >> docker_config.tmp
+    echo "Environment=\"HTTP_PROXY=http://${stmt}${http_host}:${http_port}\";" \
+         >> docker_config.tmp
+    echo "Environment=\"HTTP_PROXY=http://${stmt}${http_host}:${http_port}\";" \
+         >> docker_config.tmp
 
-	cat docker_config.tmp | sudo tee -a $CONF_FILE > /dev/null
-	rm docker_config.tmp
+    cat docker_config.tmp | sudo tee -a $CONF_FILE > /dev/null
+    rm docker_config.tmp
     sudo service docker restart
-	return
+    return
 }
 
 

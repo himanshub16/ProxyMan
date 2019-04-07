8039ecef051c30d51b84acd2b8f2f6a3ffd7225f
Himanshu Shekhar <himanshushekharb16@gmail.com>
2019-04-07 12:33:30 +0530

    use readlink instead of sed --follow-symlink

--- bash-zsh.sh
+++ bash-zsh.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+BASHRC=`readlink -f $HOME/.bashrc`
+ZSHRC=`readlink -f $HOME/.zshrc`
+
 which bash &> /dev/null
 first="$?"
 
@@ -11,7 +14,7 @@ if [ "$#" = 0 ]; then
 fi
 
 if [ "$first" = 0 ]; then
-    bash shellrc.sh "$1" "$HOME/.bashrc"
+    bash shellrc.sh "$1" "$BASHRC"
     if [ ! "$1" = "list" ]; then
         echo "To activate in current terminal window"
         echo "run ${bold}source ~/.bashrc${normal}"
@@ -19,7 +22,7 @@ if [ "$first" = 0 ]; then
 fi
 
 if [ "$second" = 0 ]; then
-    bash shellrc.sh "$1" "$HOME/.zshrc"
+    bash shellrc.sh "$1" "$ZSHRC"
     if [ ! "$1" = "list" ]; then
         echo "To activate in current terminal window"
         echo "run ${bold}source ~/.zshrc${normal}"

ad32394f2c8519e38abe37a041b0e5154deb5636
Beh <b@kayuk.de>
2020-01-25 20:57:36 +0100

    Add support for macOS in main.sh
    Create some kind of interface for different OSes:
    Main functions for setting proxies are split into different files for each OS which contain identical function names.
    The corresponding file gets sourced at the beginning of the file.

---
+++ main_macos.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+#
+# If you have found some issues, or some feature request :
+# Raise them here : https://github.com/himanshub16/ProxyMan/issues
+# Author : Benjamin Gericke (@deg0nz)
+#
+
+# This is the script that provides main script functions for macOS.
+
+function _do_it_for_selection() {
+    local what_to_do="$1"
+    for t in "${targets[@]}"
+    do
+        case "$t" in
+            # THIS stmt WILL CAUSE INFINITE RECURSION. DO NOT USE THIS.
+            # COMMENTED FOR WARNING
+            # 1) _do_it_for_all "$what_to_do"
+            #    ;;
+            2) bash "bash-zsh.sh" "$what_to_do"
+                ;;
+            3) bash "npm.sh" "$what_to_do"
+                ;;
+            3) echo "networksetup not yet implemented. skipping." #bash "networksetup.sh" "$what_to_do"
+                ;;
+            4) bash "git.sh" "$what_to_do"
+                ;;
+            *) ;;
+        esac
+    done
+}
+
+function _do_it_for_all() {
+    # Argument
+    # $1 : what to do (set/unset/list)
+
+    local what_to_do="$1"
+    if [[ -z "$targets" || "$targets" = "1" ]]; then
+
+        bash "bash-zsh.sh" "$what_to_do"
+        bash "npm.sh" "$what_to_do"
+        bash "git.sh" "$what_to_do"
+
+    else
+        _do_it_for_selection "$what_to_do"
+    fi
+}
+
+function prompt_for_proxy_targets() {
+    echo "${bold}${blue} Select targets to modify ${normal}"
+
+    echo "|${bold}${red} 1 ${normal}| All of them ... Don't bother me"
+    echo "|${bold}${red} 2 ${normal}| Terminal / bash / zsh (current user) "
+    echo "|${bold}${red} 3 ${normal}| Desktop settings (networksetup)"
+    echo "|${bold}${red} 4 ${normal}| npm & yarn"
+    echo "|${bold}${red} 5 ${normal}| Git"
+    echo
+    echo "Separate multiple choices with space"
+    echo -n "${bold} ? ${normal}" ; read targets # 
+    export targets=(`echo ${targets}`)
+}

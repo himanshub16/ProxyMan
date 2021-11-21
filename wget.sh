f587676c33d484461b949f1c8bf0c46f5b8e096b
Logesh <logeshg5@gmail.com>
2021-11-21 12:47:04 +0530

    Proxy settings with .wgetrc

--- wget.sh
+++ wget.sh
@@ -2,52 +2,73 @@
 # plugin to set "wget" proxy settings for ProxyMan
 # privileges has to be set by the process which starts this script
 
+CONF_FILE=~/.wgetrc
 
 list_proxy() {
     echo
-    echo "${bold}wget proxy settings : ${normal}"
-    local wget_bashrc_proxy=$(cat ~/.bashrc | grep -q 'wget -e use_proxy=yes')
-    local wget_zshrc_proxy=$(cat ~/.zshrc | grep -q 'wget -e use_proxy=yes')
-    if [[ $wget_bashrc_proxy -eq 0 ]]; then
-        cat ~/.bashrc | grep 'wget -e use_proxy=yes' -i | sed -e 's/^/ /'
-    elif [[ $wget_zshrc_proxy -eq 0 ]]; then
-        cat ~/.zshrc | grep 'wget -e use_proxy=yes' -i | sed -e 's/^/ /'
+    echo "${bold}wget proxy settings ${normal}"
+    lines="$(cat $CONF_FILE | grep proxy -i | cut -d"=" -f2 | wc -w)"
+    if [ "$lines" -gt 0 ]; then
+        cat $CONF_FILE | grep proxy -i 
+        # cat $CONF_FILE | grep proxy -i | sed -e 's/^/ /'
     else
         echo "${red}None${normal}"
     fi
 }
 
 unset_proxy() {
-    sed -i "/wget -e use_proxy=yes/d" ~/.bashrc
-    sed -i "/wget -e use_proxy=yes/d" ~/.zshrc
+    if [ ! -e "$CONF_FILE" ]; then
+        return
+    fi
+    # extra effort required to avoid removing custom environment variables set
+    # by the user for personal use
+    for proxytype in "http" "https" "ftp" ; do
+        sed -i "/${proxytype}_proxy\=/d" "$CONF_FILE"
+        echo "${proxytype}_proxy="     >> "$CONF_FILE"
+    done
+    for PROTOTYPE in "HTTP" "HTTPS" "FTP" ; do
+        sed -i "/${PROTOTYPE}_PROXY\=/d" "$CONF_FILE"
+        echo "${PROTOTYPE}_PROXY="     >> "$CONF_FILE"
+    done
+    sed -i "/use_proxy/d" "$CONF_FILE"
 }
 
 set_proxy() {
+    unset_proxy
+    if [ ! -e "$CONF_FILE" ]; then
+        touch "$CONF_FILE"
+    fi
+
     local stmt=""
     if [ "$use_auth" = "y" ]; then
         stmt="${username}:${password}@"
     fi
-    local wget_http_proxy="http_proxy=http://${stmt}${http_host}:${http_port}/"
-    local wget_https_proxy=""
-    local wget_ftp_proxy=""
-    # caution: do not use / after stmt
-  
-    if [ "$USE_HTTP_PROXY_FOR_HTTPS" = "true" ]; then
-        wget_https_proxy=$wget_http_proxy
-        wget_ftp_proxy=$wget_http_proxy
-    else
-        wget_https_proxy="https_proxy=https://${stmt}${https_host}:${https_port}/"
-        wget_ftp_proxy="ftp_proxy=ftp://${stmt}${ftp_host}:${ftp_port}/"
-    fi
 
-    echo "alias wget='wget -e use_proxy=yes -e $wget_http_proxy -e $wget_https_proxy -e $wget_ftp_proxy \$@'" >> ~/.bashrc
-    echo "alias wget='wget -e use_proxy=yes -e $wget_http_proxy -e $wget_https_proxy -e $wget_ftp_proxy \$@'" >> ~/.zshrc
+    for proxytype in "http" "https" "ftp"; do
+        sed -i "/${proxytype}_proxy\=/d" "$CONF_FILE"
+    done
+    for PROTOTYPE in "HTTP" "HTTPS" "FTP"; do
+        sed -i "/${PROTOTYPE}_PROXY\=/d" "$CONF_FILE"
+    done
+    sed -i "/use_proxy/d" "$CONF_FILE"
+
+    echo "use_proxy = on" >> "$CONF_FILE"
+    # caution: do not use / after stmt
+    echo "http_proxy=\"http://${stmt}${http_host}:${http_port}/\""     >> "$CONF_FILE"
+    # $https_proxy at the end
+    echo "HTTP_PROXY=\"http://${stmt}${http_host}:${http_port}/\""     >> "$CONF_FILE"
+    # $HTTPS_PROXY at the end
+
+    if [ "$USE_HTTP_PROXY_FOR_HTTPS" = "true" ]; then
+        echo "https_proxy=\"http://${stmt}${http_host}:${http_port}/\"" >> "$CONF_FILE"
+        echo "HTTPS_PROXY=\"http://${stmt}${http_host}:${http_port}/\"" >> "$CONF_FILE"
+    else
+        echo "https_proxy=\"https://${stmt}${https_host}:${https_port}/\"" >> "$CONF_FILE"
+        echo "HTTPS_PROXY=\"https://${stmt}${https_host}:${https_port}/\"" >> "$CONF_FILE"
+    fi
 }
 
 
-if [ "$#" = 0 ]; then
-    exit
-fi
 
 what_to_do=$1
 case $what_to_do in
@@ -56,7 +77,7 @@ case $what_to_do in
     unset) unset_proxy
            ;;
     list) list_proxy
-          ;;
+           ;;
     *)
-          ;;
+           ;;
 esac
